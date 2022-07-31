2c2813373d15e01cb692e7db2dae6f4f9c760916
jacob <jacob@intelliquant.net>
2022-07-31 20:45:02 +0900

    feat: first commit

---
+++ readme-generator.py
@@ -0,0 +1,75 @@
+import os
+
+
+folder_list = []
+readme_list = []
+
+for f in os.listdir(os.getcwd()):
+    if os.path.isdir(f):
+        folder_list.append(f)
+
+
+for d in folder_list:
+    os.chdir(d)
+
+    # 폴더 내 파일 목록 보기
+    file_list = os.listdir(os.getcwd())
+
+    dic = {}
+
+    for f in file_list:
+        file = f.split("_")
+
+        problem_type = file[0]
+        name = file[0] + "_" + file[1]
+        trial_count = file[2]
+        is_solved = "✅" if file[-1].split(".py")[0] == "O" else "❌"
+
+        if name in dic:
+            dic[name]["trial_result"].append(is_solved)
+        else:
+            dic[name] = {}
+            dic[name]["trial_result"] = []
+            dic[name]["trial_result"].append(is_solved)
+
+        dic[name]["problem_type"] = problem_type
+        dic[name]["name"] = name
+        dic[name]["trial_count"] = len(dic[name]["trial_result"])
+
+    os.chdir("../")
+
+    readme_list += [
+        "## " + d,
+        "| 구분 | 유형 | 문제 | 1회 풀이 | 2회 풀이 | 3회 풀이 |",
+        "| :-----: | :-----: | :-----: | :-----: | :-----: | :-----: |",
+        # "| ",
+    ]
+
+    for key in dic.keys():
+
+        problem_solve_results = dic[key]["trial_result"]
+
+        value = ["" for x in range(10)]
+
+        for i in range(len(problem_solve_results)):
+            value[i] = problem_solve_results[i]
+
+        readme_list += [
+            "| "
+            + " | ".join(
+                [
+                    d,
+                    dic[key]["problem_type"],
+                    dic[key]["name"],
+                    value[0],
+                    value[1],
+                    value[2],
+                ]
+            )
+            + " |"
+        ]
+
+
+with open("README.md", "w") as f:
+    for line in readme_list:
+        f.write(line + "\n")
