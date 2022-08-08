3262d420a3ee9075dbd781acacc1faec0758165c
jacob <talentceffort@gmail.com>
2022-08-08 23:02:29 +0900

    feat: DP_금광

--- 이코테/DP_금광_1회_X.py
+++ 이코테/DP_금광_1회_X.py
@@ -5,13 +5,32 @@ stdin = open("text/DP_금광.txt")
 
 t = int(stdin.readline())
 
-print(t)
+
+dp = []
 
 
-for x in range(t):
-    x, y = map(int, (stdin.readline().split()))
-
+for z in range(t):
+    n, m = map(int, (stdin.readline().split()))
     array = list(map(int, stdin.readline().split()))
 
-    print(x, y)
-    print(array)
+    max_value = 0
+    index = 0
+
+    for k in range(n):
+        dp.append(array[index : index + m])
+        index = index + m
+
+    for j in range(1, m):
+        for i in range(n):
+            left_up = 0 if i == 0 else dp[i - 1][j - 1]
+            left_down = 0 if i == n - 1 else dp[i + 1][j - 1]
+            left = dp[i][j - 1]
+
+            dp[i][j] = dp[i][j] + max(left_down, left_up, left)
+
+    result = 0
+
+    for i in range(n):
+        result = max(result, dp[i][m - 1])
+
+    print(result)
